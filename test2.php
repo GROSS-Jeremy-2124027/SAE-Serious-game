9f185d3e61bf0beaf21e9e149ab70d796e0ae4e2
Antoine-Welter <Antoine.Welter@etu.univ-amu.fr>
2023-01-07 21:38:06 +0100

    test2.php renvoit un int pour le score

--- test2.php
+++ test2.php
@@ -4,7 +4,26 @@ session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+include "connection/connection.php";
+$con = connect();
+
+$sql =" SELECT meilleurScore FROM `utilisateur` WHERE identifiant = '".$_SESSION["username"]."'";
+
+$result = $con->query($sql);
+
+
+
+//Fetch into associative array
+while ( $row = $result->fetch_assoc())  {
+    $dbdata=$row;
+}
+
 
 //Print array in JSON format
-echo json_encode($_SESSION['username']);
+
+/* current() sert à avoir l'index actuel donc dans ce cas la derniere case de $dbdata car on l'a parcouru dans la boucle while
+*  au-dessus, le reste sert à transformer la valeur de score de string a int
+*/
+echo json_encode((int) filter_var(current($dbdata), FILTER_SANITIZE_NUMBER_INT));
 ?>
+    
