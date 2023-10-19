723ba0e5a2425cca70d355a86b02739d36117955
Jeremy <jeremy.gross@etu.univ-amu.fr>
2023-10-19 14:02:00 +0200

    fix

--- godot/godot-game/Fullscreen.gd
+++
@@ -1,4 +0,0 @@
-extends TouchScreenButton
-
-func _on_Fullscreen_pressed():
-	OS.window_fullscreen = !OS.window_fullscreen
--- godot/godot-game/unused_Sombre.gd
+++
@@ -1,9 +0,0 @@
-extends ColorRect
-
-var BLACK = Color(0,0,0)
-var completed = false
-
-func _on_Sombre_visibility_changed():
-	if (!visible and !completed):
-		for i in get_child_count():
-			get_child(i).get_node("ColorRect").color = BLACK
